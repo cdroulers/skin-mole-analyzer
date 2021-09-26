71b45e9c373f66bd16d2e8baa5799846ff5e2e06
Christian Droulers <droulers.christian@gmail.com>
2021-09-26 13:33:11 -0400

    Barebones app skeleton!

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false
+}
